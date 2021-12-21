6d6e726335ecec4f4d0268f9c715411862b7d9b3
Mohamed7499-Salama <mosalama7499@gmail.com>
2021-12-22 00:16:28 +0200

    Hello!

---
+++ appdoc.java
@@ -0,0 +1,79 @@
+
+
+
+
+public class appdoc 
+{
+
+  public  static void printval(int x)
+  {
+      if(x<0)
+          return;
+      for(int i=x;i>=0;i--)
+            System.out.println(i);
+      
+  }
+
+  public static int []createarray(int size)
+  {
+      System.out.println("enter array size");
+      
+      
+      int arr[]=new int [size];
+      
+  return arr;
+  
+  }
+    public static void main(String[] args){ 
+    
+//        Car kia=new Car(50);
+//        kia.Incspeed(250);
+//        
+//        int x=50;
+//        int y=(x==50) ? ((x==20) ? 5 :2) : 30;
+//        
+//        System.out.println(y);
+        
+//        printval(10);
+//        
+//        int values[]=createarray(5);
+//        
+       
+          //Multi dimintion array
+          
+//          int x[][]={{1,2},
+//                     {3,4},
+//                     {5,6}};    
+//          int c=3,z=5;
+//          int y[][]=new int[c][z];
+//          y[0][0]=10;
+//          y[0][1]=20;
+//          y[0][2]=30;
+//          y[0][3]=40;
+//          y[0][4]=50;
+//          
+//          y[1][0]=60;
+//          y[1][1]=70;
+//          y[1][2]=80;
+//          y[1][3]=90;
+//          y[1][4]=100;
+//          
+//          y[2][0]=110;
+//          y[2][1]=120;
+//          y[2][2]=130;
+//          y[2][3]=140;
+//          y[2][4]=150;
+//          
+//          System.out.println(y[1][3]);
+     
+          
+          
+          
+          
+          
+
+    }
+
+   
+    
+}
